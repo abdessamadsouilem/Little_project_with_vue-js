0bd8f657e556b4c81476d4d12fe2935cf2cd48c0
abdessamad <abdessamadsouilem1@gmail.com>
2021-04-15 14:47:33 +0000

    add first prject

---
+++ workshop/src/App.vue
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
+<template>
+  <div>
+<SearchBar @termChange="onTermChange"></SearchBar>
+<VideoList></VideoList>
+  </div>  
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
+
+const API_KEY = 'AIzaSyAYY8ihb_BoBVvWwB0TxoUmWKP7dBFy7zs';
+export default {
+components: { SearchBar,
+VideoList },
+name: 'App',
+methods:{
+    onTermChange(searchTearm){
+        axios
+        .get('https://www.googleapis.com/youtube/v3/search', {
+            params: {
+                key: API_KEY,
+                type: 'video',
+                part: 'snippet',
+                q: searchTearm
+            }
+        }).then(response => console.log(response));
+    }
+}
+};
+</script>
---
+++ workshop/src/components/VideoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        VedioList
+    </ul>
+</template>
+
+
+<script>
+export default {
+    name: 'VideoList'
+};
+
+</script>
+
+<style>
+
+</style>
